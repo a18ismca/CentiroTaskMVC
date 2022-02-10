519b131853cc13130824c64f69cddf368075b1e5
a18ismca <a18ismca@student.his.se>
2022-02-10 19:24:27 +0100

    Finished with showing all orders from the database. Next step is to create a dropdown list where you can search for orders. Moreover, a test will be created to test the code.

---
+++ CentiroTaskMVC/CentiroTaskMVC/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CentiroTaskMVC.Models;
+
+namespace CentiroTaskMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        // GET: /Order/GetAll
+        public IActionResult GetAll()
+        {
+            var dbConnection = new Connect();
+            ViewBag.AllOrderTable = dbConnection.GetAllOrders();
+
+            return View();
+        }
+
+        public IActionResult OrderNumberSearch(string orderNumber)
+        {
+            var dbConnection = new Connect();
+            ViewBag.SpecificOrderTable = dbConnection.GetSpecificOrderNumber(orderNumber);
+            return View();
+        }
+    }
+}
---
+++ CentiroTaskMVC/CentiroTaskMVC/Models/Connect.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentiroTaskMVC.Models
+{
+    public class Connect
+    {
+        
+            private readonly string ConnectionString = "Server=localhost;Database=centirodb;User ID=root;Password=Pridvorci99;Pooling=false;SslMode=none;convert zero datetime=True;";
+
+
+            public DataTable GetAllOrders()
+            {
+                MySqlConnection dbcon = new MySqlConnection(ConnectionString);
+                dbcon.Open();
+                MySqlDataAdapter adapter = new MySqlDataAdapter("select * from CentiroOrder order by OrderNumber;", dbcon);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "result");
+                DataTable orderTable = ds.Tables["result"];
+                dbcon.Close();
+
+                return orderTable;
+
+            }
+
+        public DataTable GetSpecificOrderNumber(string orderNumber)
+        {
+
+            MySqlConnection dbcon = new MySqlConnection(ConnectionString);
+            dbcon.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("select * from CentiroOrder where OrderNumber=@OrderNumber;", dbcon);
+            adapter.SelectCommand.Parameters.AddWithValue("@OrderNumber", orderNumber);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable specificOrderTable = ds.Tables["result"];
+            dbcon.Close();
+
+            return specificOrderTable;
+
+        }
+
+       
+    }
+}
